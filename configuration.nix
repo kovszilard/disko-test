b0b43e9ae8ae020f329742f8ed0df6f9a49f94b2
Szilard Kovacs <kovszilard@gmail.com>
2024-09-20 22:40:33 +0200

    hardware config

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
-      ./disk-config.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
