ce30c42b8e19e8df6e8858ae9c6dcc945c1f3dde
Szilard Kovacs <kovszilard@gmail.com>
2024-09-20 21:29:08 +0200

    init

---
+++ disko.nix
@@ -0,0 +1,69 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        # device = "/dev/vdb";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              start="1M";
+              size = "5G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ ];
+                passwordFile = "/tmp/secret.key"
+                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "lvm_pv";
+                  vg = "enc-pv";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      enc-pv = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+}
